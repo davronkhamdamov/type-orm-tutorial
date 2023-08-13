a147de8e1240736dba90cdf577883c9a6217a2b3
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-08-13 19:48:23 +0500

    Initilize commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '../ormconfig';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(config)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
